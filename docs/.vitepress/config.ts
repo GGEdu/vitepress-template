66fcaffa36ea5d289639320eff25e3e27628627d
GGarrido <g.garridoportes@edu.gva.es>
2025-05-13 19:48:50 +0200

    chore: restaurar archivo de configuración de VitePress y agregar contenido inicial a la documentación

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
   base: '/vitepress-template/',
+  srcDir: 'docs',
 })
---
+++ docs/docs/index.md
@@ -0,0 +1,5 @@
+# Hello World
+
+¡Bienvenido a la documentación de tu proyecto VitePress!
+
+Puedes editar este archivo para personalizar la página principal.
