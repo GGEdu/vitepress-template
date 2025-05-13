ffe3f17abae4c53441dc5db747f1355bf22e4447
GGarrido <g.garridoportes@edu.gva.es>
2025-05-13 19:13:27 +0200

    chore: initialize VitePress project with package.json

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+  // base: '/', // Usa esto para desarrollo local
+  base: '/vitepress-template/', // Cambia a '/<REPO_NAME>/' para despliegue en GitHub Pages
+})
